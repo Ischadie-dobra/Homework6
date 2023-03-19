2e022db874cc511d6237d9d6acec3826e631cd9e
Ischadie-dobra <125760331+Ischadie-dobra@users.noreply.github.com>
2023-03-19 13:23:35 +0300

    Домашняя работа 6

---
+++ Program.cs
@@ -0,0 +1,72 @@
+﻿//Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+//0, 7, 8, -2, -2 -> 2
+//1, -7, 567, 89, 223-> 3
+
+/*
+int Prompt(string massage)
+{
+    Console.WriteLine(massage);
+    string value = Console.ReadLine();
+    int result = Convert.ToInt32(value);
+    return result;
+}
+
+int[] MyArray(int size)
+{
+int[] array = new int[size];
+Random rnd = new Random();
+for(int i = 0; i < array.Length; i++)
+{
+array[i] = rnd.Next(-100, 100);
+}
+return array;
+}
+void PrintArray(int[] array)
+{
+    for(int i = 0; i < array.Length; i++)
+    {
+     Console.WriteLine($"a[{i}] = {array[i]}");
+    }
+}
+int CountPositivNumbers(int[] array)
+{
+    int count = 0;
+    for(int i = 0; i < array.Length; i++)
+    {
+        if(array[i] > 0)
+        {
+            count++;
+        }
+    }
+    return count;
+}
+int length = Prompt("Введите число");
+int[] array;
+array = MyArray(length);
+PrintArray(array);
+Console.WriteLine($"Количество чисел больше 0 - {CountPositivNumbers(array)}");
+*/
+
+//Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, 
+//заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+//b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+Console.Write("Введите k1: ");
+var k1 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите b1: ");
+var b1 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите k2: ");
+var k2 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите b2: ");
+var b2 = Convert.ToDouble(Console.ReadLine());
+ 
+ 
+var x = -(b1 - b2) / (k1 - k2);
+var y = k1 * x + b1;
+ 
+x = Math.Round(x, 3);
+y = Math.Round(y, 3);
+ 
+Console.WriteLine($"Пересечение в точке: ({x};{y})");
+
+
